359400477ecf0f336503b1c7855586fe36e89b46
Geoffrey Gordon Ashbrook <lineality@gmail.com>
2023-09-14 18:00:12 -0400

    Update terminal_print_variations.py

--- terminal_print_variations.py
+++ terminal_print_variations.py
@@ -33,17 +33,17 @@ def typed_print(input_text):
     random_times = [
         0.03,
         0.04,
-        0.04,
-        0.05,
-        0.05,
         0.05,
         0.06,
         0.07,
-        0.07,
         0.08,
         0.08,
         0.08,
+        0.08,
+        0.09,
         0.1,
+        0.2,
+        0.2,
         0.3,
     ]
     # stagger each character
